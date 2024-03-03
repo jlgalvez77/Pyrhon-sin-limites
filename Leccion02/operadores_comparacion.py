626e52fbaf85d8ae0a35e604460b6b338b738d8b
Jose Galvez <jlgalvez77@gmail.com>
2024-03-03 23:29:15 +0100

    03/03/24-noche

---
+++ Leccion02/operadores_comparacion.py
@@ -0,0 +1,20 @@
+a = 4
+b = 2
+
+resultado = a == b
+print(resultado)
+
+resultado = a != b
+print(resultado)
+
+resultado = a > b
+print(resultado)
+
+resultado = a >= b
+print(resultado)
+
+resultado = a < b
+print(resultado)
+
+resultado = a <= b
+print(resultado)
